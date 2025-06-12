8bfdd5f57d66b996077c118d7b8d40b10a9ed9ab
yaascavalcanti <yasmnin.g.barros6@aluno.senai.br>
2025-06-12 07:58:57 -0300

    novo

--- script.js
+++ script.js
@@ -9,8 +9,35 @@ let nome = prompt("Qual o seu nome?")
 titulo.innerHTML = `Lista de tarefa: ${nome}`;
 
 // acompanha o evento de clque do botão adicionar tarefa
-btnAdd.addEventListener("click]",criaTarefa);
+btnAdd.addEventListener("click",criaTarefa);
+
 
 function criaTarefa(){
-    
+    const listItem = document.createElement('li');
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem);
+
+    // criar botão de deletar tarefa
+    const removeButton = document.createElement('button');
+    removeButton.id = "remove";
+    removeButton.textContent = "x"
+    /*listItem.appendChild(removeButton);*/
+
+    removeButton.addEventListener("click", function(){
+        taskList.removeChild(listItem);
+    })
+    // criar botãoo de concluir tarefa
+    const concluirButton = document.createElement('button');
+    concluirButton.id = "conclui";
+    concluirButton.textContent = "✓";
+    /*listItem.appendChild(concluirButton);*/
+
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonItem');
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
+    concluirButton.addEventListener("click", function(){
+        listItem.classList.toggle('completed');
+    })
 }
